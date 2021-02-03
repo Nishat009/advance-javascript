2d640bf2d4873ffde57f73d8d9e534124391d36d
Nishat009 <nishatraihana009@gmail.com>
2021-02-03 23:09:52 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,16 @@
+
+function sum (first, second){
+    let result=first+second;
+    if(result>9){
+        var mood="happy";
+        mood="cranky";
+        mood="fanky";
+        mood="fishy";
+       
+    }
+
+
+console.log(day);
+let day='friday';
+return result;
+}
